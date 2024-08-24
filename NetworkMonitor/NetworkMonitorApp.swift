76887e4b90f18fbc612fa08b3ba1d0a9cb1adc9c
Satyadev Chauhan <satyadevchauhan@gmail.com>
2024-08-24 19:33:40 +0530

    Initial Commit

---
+++ NetworkMonitor/NetworkMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitorApp.swift
+//  NetworkMonitor
+//
+//  Created by Satyadev Chauhan on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
